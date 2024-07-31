1f721cb1eca78d304050af0900f572229a8d419e
JamDon2 <25279396+JamDon2@users.noreply.github.com>
2024-07-31 14:27:11 +0200

    add compare script

---
+++ compare.py
@@ -0,0 +1,31 @@
+from arena import Strategy, Game
+from util import import_strategies
+
+ROUNDS = 10000
+
+strategies = import_strategies("strategies")
+
+
+for i, strategy in enumerate(strategies):
+    print(f"{i+1}. {strategy.name}")
+
+first = int(input("\nWhich is your first strategy? ")) - 1
+second = (
+    int(
+        input(
+            f"\nWhich strategy would you like to compare {strategies[first].name} against? "
+        )
+    )
+    - 1
+)
+
+strategy1 = strategies[first]
+strategy2 = strategies[second]
+
+game = Game(strategy1(), strategy2())
+
+win1, win2, tie = game.play_rounds(ROUNDS)
+
+print(
+    f"\n{strategy1.name} won {win1/ROUNDS:.2%} of rounds, it lost {win2/ROUNDS:.2%}, and tied on {tie/ROUNDS:.2%}."
+)
